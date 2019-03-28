76d16bcde17cb6e5396968e06f7ce80b2fc3e920
Josh Haws <joshhaws0@gmail.com>
2019-03-28 00:21:17 -0500

    closes #1

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -7,36 +7,36 @@ class Register extends Component {
     render() {
         return(
             <div className="body-content">
-                <div className="login-container">
-                    <div className="login-title">
+                <div className="register-container">
+                    <div className="reset-title">
                         <img src="logo.svg" alt="logo" />
                     </div>
-                    <div className="login-message">
+                    <div className="register-message">
                         <div className="welcome">Welcome.</div>
                         <div className="welcome-sub">Sign up for awesomeness.</div>
                     </div>
-                    <div className="login-form-container">
+                    <div className="register-form-container">
                         <div className="register-form-links">
                             <Link className="login-link" to="/login">Login?</Link>
                         </div>
                         <form>
-                            <div className="email-login-input-container">
+                            <div className="email-register-input-container">
                                 <div className="email-input-label">Email</div>
                                 <input className="email-input-field" placeholder="you@email.com"></input>
                             </div>
                             
-                            <div className="password-login-input-container">
+                            <div className="password-register-input-container">
                                 <div className="password-input-label">Password</div>
                                 <input className="password-input-field" placeholder="Password"></input>
                             </div>
 
-                            <div className="password-login-input-container">
+                            <div className="password-register-input-container">
                                 <div className="password-input-label">Confirm Password</div>
                                 <input className="password-input-field" placeholder="Password"></input>
                             </div>
                             
-                            <div className="login-button-container">
-                                <input type="submit" className="login-button" value="Sign up for Simplifi" />
+                            <div className="register-button-container">
+                                <input type="submit" className="register-button" value="Sign up for Simplifi" />
                             </div>
                         </form>
                     </div>
--- src/pages/ResetPassword/ResetPassword.js
+++ src/pages/ResetPassword/ResetPassword.js
@@ -7,27 +7,27 @@ class ResetPassword extends Component {
     render() {
         return(
             <div className="body-content">
-                <div className="login-container">
-                    <div className="login-title">
+                <div className="reset-container">
+                    <div className="reset-title">
                         <img src="logo.svg" alt="logo" />
                     </div>
-                    <div className="login-message">
+                    <div className="reset-message">
                         <div className="welcome">Oh No.</div>
                         <div className="welcome-sub">Input email for password reset.</div>
                     </div>
-                    <div className="login-form-container">
+                    <div className="reset-form-container">
                         <div className="reset-form-links">
                             <Link className="register-link" to="/register">Need an Account?</Link>
                             <Link className="login-link" to="/login">Login?</Link>
                         </div>
                         <form>
-                            <div className="email-login-input-container">
+                            <div className="email-reset-input-container">
                                 <div className="email-input-label">Email</div>
                                 <input className="email-input-field" placeholder="you@email.com"></input>
                             </div>
                             
-                            <div className="login-button-container">
-                                <input type="submit" className="login-button" value="Send Reset Email" />
+                            <div className="reset-button-container">
+                                <input type="submit" className="reset-button" value="Send Reset Email" />
                             </div>
                         </form>
                     </div>
