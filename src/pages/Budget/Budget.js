ef705b5bb76ebccec142f7336c1e6e2033bd51c9
Josh Haws <joshhaws0@gmail.com>
2019-03-28 23:45:00 -0500

    progress

--- src/pages/Budget/Budget.js
+++ src/pages/Budget/Budget.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react';
 import './Budget.scss';
 
 class Budget extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: [
+                {
+                    name: 'Monthly Obligations',
+                    budgets: [
+                        {
+                            name: 'grocery',
+                            amount: 500
+
+                        },
+                        {
+                            name: 'transporation',
+                            amount: 150
+                        }
+                    ]
+                },
+                {
+                    name: 'Non-Monthly',
+                    budgets: [
+                        {
+                            name: 'prime',
+                            amount: 10
+
+                        },
+                        {
+                            name: 'insurance',
+                            amount: 200
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+
     render() {
         return(
             <div className="budget-container">
                 <div className="item budget-header">Header</div>
-                <div className="item budget-content">Content</div>
+                <div className="item budget-content">
+                    <div>{ this.state.categories.map(category => <div>{category.name}</div>) }</div>
+                </div>
                 <div className="item budget-sidebar">Sidebar</div>
             </div>
+            
         )
     }
 }
