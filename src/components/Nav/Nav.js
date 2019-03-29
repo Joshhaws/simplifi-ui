143af0d24936ef2e1fb0bc3e62d7de8c32d91dbc
Tanner Sawyer <tanner.sawyer@gmail.com>
2019-03-29 13:36:02 -0600

    add in colors.scss, hamburger menu and animations, added react-icons, some layout changes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
 import './Nav.scss';
 import { Link } from 'react-router-dom';
-
 class Nav extends Component {
-    render(){
+
+    state = {
+        windowWidth : window.innerWidth,
+        toggle : false,
+        closing : false
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    }
+    
+    handleResize = () => {
+        this.setState({windowWidth: window.innerWidth});
+    }
+    
+    handleToggle = () => {
+        const { toggle } = this.state
+        if(toggle){
+            this.setState({ closing : true }, () => {
+                setTimeout(()=> {
+                    this.setState(prevState => ({ 
+                        toggle : !prevState.toggle,
+                        closing : false
+                    }))
+                }, 300)
+            })
+        }
+        else{
+            this.setState(prevState => ({ toggle : !prevState.toggle }))
+        }
+    }
+
+    mobileNavigationLinks = () => {
+        const { toggle, closing } = this.state
+        return (
+            <div className='mobile-nav'>
+                <div className='mobile-nav__left'>
+                    <Link to ="/" className="mobile-nav__left-title">
+                        <img className="mobile-nav__left-title-logo" src="logo.svg" alt="logo" />
+                    </Link>
+                </div>
+                <div className='mobile-nav__right'>
+                    <div class={`hamburger hamburger--slider ${toggle ? ' is-active': ''}`} onClick={this.handleToggle}>
+                        <div class='hamburger-box'>
+                            <div class='hamburger-inner'></div>
+                        </div>
+                    </div>
+                    { toggle && 
+                        <div className={`mobile-nav__right-links ${closing ? ' closing' : ''}`}>
+                            <Link to='/about' className='mobile-nav__right-links-link'>About</Link>
+                            <Link to='/contact' className='mobile-nav__right-links-link'>Contact</Link>
+                            <Link to='/register' className='mobile-nav__right-links-link'>Register</Link>
+                            <Link to='/login' className='mobile-nav__right-links-link' onClick={this.handleToggle}>Login</Link>
+                        </div>
+                    }
+                </div>
+            </div>
+        )
+    }
+
+    mainNavigationLinks = () => {
         return(
             <div className="main-nav">
                 <div className="main-nav-left">
@@ -17,6 +80,16 @@ class Nav extends Component {
                     <Link to="/register" className="main-nav-sign-up">Sign Up</Link>
                     <Link to="/login" className="main-nav-login">Login</Link>
                 </div>
+            </div>
+        )
+    }
+
+    render(){
+        const { windowWidth } = this.state
+        console.log(windowWidth)
+        return(
+            <div className='nav-container'>
+                { windowWidth <= 768 ? this.mobileNavigationLinks() : this.mainNavigationLinks() }
             </div>
         )
     }
