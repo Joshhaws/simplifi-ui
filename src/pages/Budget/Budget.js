52dd343e5d87ebed51d15b154234a7c616078062
Josh Haws <joshhaws0@gmail.com>
2019-03-30 00:58:28 -0500

    adding table for budget concept

--- src/pages/Budget/Budget.js
+++ src/pages/Budget/Budget.js
@@ -2,26 +2,12 @@ import React, { Component } from 'react';
 import './Budget.scss';
 
 class Budget extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            categories: [
-                {
-                    name: 'Monthly Obligations',
-                    budgets: [
-                        {
-                            name: 'grocery',
-                            amount: 500
-
-                        },
-                        {
-                            name: 'transporation',
-                            amount: 150
-                        }
-                    ]
-                }
-            ]
-        };
+    state = {
+        sidebar: 'stuff'
+    }
+ 
+    selectCategory = (val) => {
+        this.setState({sidebar: val});
     }
 
     render() {
@@ -29,11 +15,203 @@ class Budget extends Component {
             <div className="budget-container">
                 <div className="item budget-header">Budget</div>
                 <div className="item budget-content">
-                    <div>
-                        { this.state.categories.map(category => <div>{category.name}</div>) }
+                    <div className="category-container">
+                        {/* TODO - we should build this out as component... feeling tired so I'll do it later :) lol */}
+                        <div className="category-group-title-container" onClick={() => this.selectCategory('monthly obligations')}>
+                            <div className="category-group-title">Monthly Obligations</div>
+                            <div className="category-group-budgeted">$4,000</div>
+                            <div className="category-group-activity">$3,500</div>
+                            <div className="category-group-available">$500</div>
+                        </div>
+                        <div className="category-item" onClick={() => this.selectCategory('friggin groceries')}>
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-group-title-container">
+                            <div className="category-group-title">Monthly Obligations</div>
+                            <div className="category-group-budgeted">$4,000</div>
+                            <div className="category-group-activity">$3,500</div>
+                            <div className="category-group-available">$500</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-group-title-container">
+                            <div className="category-group-title">Monthly Obligations</div>
+                            <div className="category-group-budgeted">$4,000</div>
+                            <div className="category-group-activity">$3,500</div>
+                            <div className="category-group-available">$500</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-group-title-container">
+                            <div className="category-group-title">Monthly Obligations</div>
+                            <div className="category-group-budgeted">$4,000</div>
+                            <div className="category-group-activity">$3,500</div>
+                            <div className="category-group-available">$500</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
+                        <div className="category-item">
+                            <div className="category-title">Grocery</div>
+                            <div className="category-budgeted">$500</div>
+                            <div className="category-activity">$300</div>
+                            <div className="category-available">$200</div>
+                        </div>
                     </div>
                 </div>
-                <div className="item budget-sidebar">Sidebar</div>
+                <div className="item budget-sidebar">{this.state.sidebar}</div>
             </div>
 
         )
