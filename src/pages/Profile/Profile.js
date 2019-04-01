b798db31eccb4d3a83b84625e7c8ab7f4cf24bbd
Josh Haws <joshhaws0@gmail.com>
2019-03-31 19:46:04 -0500

    added basic profile page

---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Profile.scss';
+
+class Profile extends Component {
+    render() {
+        return(
+            <div className="body-content">Profile</div>
+        )
+    }
+}
+
+export default Profile;
