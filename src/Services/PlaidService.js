d8967a3650e89e5d77782561c03a8a4898906bbc
Josh Haws <joshhaws0@gmail.com>
2019-04-11 00:44:11 -0500

    creating plaid service to integrate with account addition

---
+++ src/Services/PlaidService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+let helpers = {
+  get_access_token : (public_token) => {
+    return axios.post(
+      `${process.env.REACT_APP_API}/api/get_access_token`,
+       { headers: 
+        {
+          'Content-Type': 'application/json'
+        }, 
+        "public_token": public_token
+      })
+      .then(res => {
+        return res;
+      });
+  }
+}
+
+export default helpers;
